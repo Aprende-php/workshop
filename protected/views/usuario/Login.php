82e9167b8727a5274c05a2d82b4a34a4c8b04c72
Ruben <rbntejeda@hotmail.com>
2015-12-30 12:47:50 -0300

    Reparacion Recordar y cambio idioma

--- protected/views/usuario/Login.php
+++ protected/views/usuario/Login.php
@@ -1,16 +1,17 @@
 <?php
+
 $baseUrl=Yii::app()->baseUrl;
 Yii::app()->getClientScript()
     ->registerScriptFile($baseUrl.'/js/jquery.Rut.min.js',CClientScript::POS_END)
     ->registerScript('ValidaRutLogin', "$('#LoginForm_username').Rut({
-    	on_error: function(){ alert('El rut ingresado es incorrecto'); }
+      on_error: function(){ alert('El rut ingresado es incorrecto'); }
 })
 ");
 ?>
 <style type="text/css">
-	body {
+  body {
     width:100px;
-	height:100px;
+  height:100px;
   background: -webkit-linear-gradient(90deg, #16222A 10%, #3A6073 90%); /* Chrome 10+, Saf5.1+ */
   background:    -moz-linear-gradient(90deg, #16222A 10%, #3A6073 90%); /* FF3.6+ */
   background:     -ms-linear-gradient(90deg, #16222A 10%, #3A6073 90%); /* IE10 */
@@ -20,15 +21,15 @@ font-family: 'Raleway', sans-serif;
 }
 
 p {
-	color:#CCC;
+  color:#CCC;
 }
 
 .spacing {
-	padding-top:7px;
-	padding-bottom:7px;
+  padding-top:7px;
+  padding-bottom:7px;
 }
 .middlePage {
-	width: 680px;
+  width: 680px;
     height: 500px;
     position: absolute;
     top:0;
@@ -39,7 +40,7 @@ p {
 }
 
 .logo {
-	color:#CCC;
+  color:#CCC;
 }
 </style><link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
 
@@ -58,23 +59,26 @@ p {
   <div class="row">
   
 <div class="col-md-5" >
-	<?= BsHtml::lead('Si desea conocer su evaluacion favor hacer click '.BsHtml::button('Aquí', array('color' => BsHtml::BUTTON_COLOR_LINK,'onclick'=>"location.href='Link Evaluacion'")));?>
+  <?= BsHtml::lead('Si desea conocer su evaluacion favor hacer click '.BsHtml::button('Aquí', array('color' => BsHtml::BUTTON_COLOR_LINK,'onclick'=>"location.href='Link Evaluacion'")));?>
 
 </div>
 
-    <div class="col-md-7" style="border-left:1px solid #ccc;height:160px">
+    <div class="col-md-7" style="border-left:1px solid #ccc">
 <fieldset>
 <?php $form=$this->beginWidget('bootstrap.widgets.BsActiveForm', array(
-    'layout' => BsHtml::FORM_LAYOUT_HORIZONTAL,
     'enableClientValidation'=>true,
 )); ?>
 
 <?= $form->textField($model, 'username', array('prepend' => BsHtml::icon(BsHtml::GLYPHICON_USER),'placeholder'=>'Rut','required'=>'required'));?><br>
-<?= $form->passwordField($model, 'password', array('prepend' => BsHtml::icon(BsHtml::GLYPHICON_LOCK),'placeholder'=>'Contraseña','required'=>'required'));?>
-<?= $form->checkBoxList($model, 'rememberMe', array(1=>"Recuerdame"));?>
+<?= $form->passwordField($model, 'password', array('prepend' => BsHtml::icon(BsHtml::GLYPHICON_LOCK),'placeholder'=>'Contraseña','required'=>'required'));?><br>
+<div class="input-group">
+<?= $form->errorSummary($model); ?>
+</div>
+<?= $form->checkBoxControlGroup($model, 'rememberMe');?>
 <?= BsHtml::submitButton('Acceder', array('color' => BsHtml::BUTTON_COLOR_INFO,'pull' => BsHtml::PULL_RIGHT
 ));
 ?>
+  
 <?php $this->endWidget();?>
 </fieldset>
 </form>
