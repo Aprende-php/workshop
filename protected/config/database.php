9950cf12995cec73d332cf5f61fb174a3747ec3c
Ruben <rbntejeda@hotmail.com>
2016-01-05 22:57:55 -0300

    asd

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,10 +1,17 @@
 <?php
-
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'mysql:host=localhost;dbname=Qualitat',
+	'connectionString' => 'mysql:host=localhost;dbname=ko000361_workshop_v2',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '',
+	'password' => '164352',
 	'charset' => 'utf8',
 );
+// // This is the database connection configuration.
+// return array(
+// 	'connectionString' => 'mysql:host=localhost;dbname=Qualitat',
+// 	'emulatePrepare' => true,
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'charset' => 'utf8',
+// );
